9e93345e8722be9138538197d6e561ff9ffdc814
MarcRoemmelt <mail@m-roemmelt.com>
2022-03-23 21:06:24 +0100

    chore: update project configuration

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,3 @@
 const nxPreset = require('@nrwl/jest/preset');
 
-module.exports = { ...nxPreset, testMatch: ['**/+(*.)+(e2e-spec|e2e-test|spec|test).+(ts|js)?(x)'], };
+module.exports = { ...nxPreset, testMatch: ['**/+(*.)+(e2e-spec|e2e-test|spec|test).+(ts|js)?(x)'] };
